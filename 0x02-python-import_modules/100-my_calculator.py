a4ce0fc0875677cd0faf56166b016c96a74aebb8
Ibidapo-chad <Ibidapo.chad@gmail.com>
2021-05-23 18:13:53 +0000

    create: program code for project task written for file

---
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -0,0 +1,21 @@
+from calculator_1 import add, sub, mul, div
+from sys import argv
+if __name__ == "__main__":
+    arg_num = len(argv) - 1
+    if arg_num != 3:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        exit(1)
+    elif argv[2] not in "+-*/":
+        print("Unknown operator. Available operators: +, -, * and /")
+        exit(1)
+    else:
+        a = int(argv[1])
+        b = int(argv[3])
+        if argv[2] == "+":
+            print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
+        elif argv[2] == "-":
+            print("{:d} - {:d} = {:d}".format(a, b, sub(a, b)))
+        elif argv[2] == "*":
+            print("{:d} * {:d} = {:d}".format(a, b, mul(a, b)))
+        elif argv[2] == "/" and b != 0:
+            print("{:d} / {:d} = {:d}".format(a, b, div(a, b)))
---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+from sys import argv
+if __name__ == "__main__":
+    arg_num = len(argv)
+    if arg_num == 1:
+        print("{} arguments.".format(arg_num - 1))
+    elif arg_num == 2:
+        print("{} argument:".format(arg_num - 1))
+    else:
+        print("{} arguments:".format(arg_num - 1))
+
+    if arg_num >= 2:
+        for i in range(1, arg_num):
+            print("{}: {}".format(i, argv[i]))
